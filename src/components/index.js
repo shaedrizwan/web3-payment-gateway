fac29e1209f2c30a65c0b7486a649444dfca1026
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-11-23 00:45:28 +0530

    Feat: components index updated

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import PaymentHome from "./PaymentHome/PaymentHome";
+import PayViaEth from "./PayViaEth/PayViaEth";
+import PayViaCard from "./PayViaCard/PayViaCard";
+
+export {PaymentHome,PayViaEth,PayViaCard}
