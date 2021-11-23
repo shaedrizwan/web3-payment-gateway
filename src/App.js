5d89f09ee555fd95f3afb5851cd99a7d4d8c3dfc
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-11-23 14:02:05 +0530

    Feat: Demo Product details added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import PaymentGateway from './components/PaymentGateway/PaymentGateway';
 import MerchantLogo from "./assets/merchantLogo.png";
+import TshirtImage from "./assets/wagmi-tshirt.png";
 
 function App() {
 
@@ -19,19 +20,27 @@ function App() {
   
 
   return (
-    <>
-    <div><button onClick={()=>setPaymentModal(true)}>Show payment</button></div>
-    {paymentModal && <PaymentGateway 
-    price={3599}
-    productName="T-Shirt"
-    merchantName="Blue Ribbon Sports"
-    merchantLogo={MerchantLogo}
-    ethAddress="0xcB38a7C0a3BE1cd1AE2F737221Dbc3400F5480Ab"
-    closeModal={closeModal}
-    successFunction={successFunction}
-    failureFunction={failureFunction}
-    />}
-  </>
+    <div className="home">
+      <div className="image-container">
+        <img src={TshirtImage} alt="tshirt"/>
+      </div>
+      <div className="pr-details">
+        <div className="pr-title">WAGMI T-Shirt - Limited Edition</div>
+        <div className="pr-price">₹ 3599</div>
+        <div className="pr-description">Click the Buy Now button to open the payment component. Please note that this is a demo payment gateway. Do not use Ethereum Mainnet or Real Card Details for the transaction.</div>
+        <div><button className="btn-buynow" onClick={()=>setPaymentModal(true)}>Buy Now</button></div>
+      </div>
+      {paymentModal && <PaymentGateway 
+      price={3599}
+      productName="T-Shirt"
+      merchantName="Blue Ribbon Sports"
+      merchantLogo={MerchantLogo}
+      ethAddress="0xcB38a7C0a3BE1cd1AE2F737221Dbc3400F5480Ab"
+      closeModal={closeModal}
+      successFunction={successFunction}
+      failureFunction={failureFunction}
+      />}
+  </div>
   );
 }
 
